f0738e5680f0405516fdbebbf3cfc614eb855a3f
Jack Ratcliff <jratcliff@sencha.com>
2018-10-30 11:35:35 -0500

    Added Ext 4.2.6 sample app

---
+++ sampleapps/ext426app/app/Application.js
@@ -0,0 +1,170 @@
+Ext.define('overrides.AbstractComponent', {
+    override: 'Ext.AbstractComponent',
+
+    /**
+     * Override that will reuse any generated ids if they exist
+     */
+    getAutoId: function () {
+        var xtype = this.xtype,
+            autoIdReuseCache = Ext.autoIdReuseCache,
+            reuseId;
+
+        if (Ext.reuseAutoIds && autoIdReuseCache && autoIdReuseCache[xtype] && autoIdReuseCache[xtype].length > 0) {
+            reuseId = autoIdReuseCache[xtype].pop();
+            console.log('Reusing auto-id:' + reuseId);
+            return reuseId.split('-')[1]; // return the number part
+        } else {
+            return this.callParent();
+        }
+    },
+
+    /**
+     * Override that will keep track of auto-generted ids as components
+     * are destroyed so that these ids can be re-used and thus help reduce
+     * a dynamic id memory leak bug in IE.
+     */
+    destroy: function () {
+        var me = this,
+            id = me.id,
+            idArray = Ext.reuseAutoIds && Ext.isString(id) ? id.split('-') : false,
+            xtype = idArray && idArray.length === 2 ? idArray[0] : undefined,
+            autoId = idArray && idArray.length === 2 ? idArray[1] : undefined;
+
+        if (!Ext.autoIdReuseCache) {
+            Ext.autoIdReuseCache = {};
+        }
+
+        // only save the id if is in the in the format of {xtype}-{number}
+        if (xtype && Ext.isNumeric(autoId)) {
+            console.log('Saving auto-id: ' + id);
+            if (!Ext.autoIdReuseCache[xtype]) {
+                Ext.autoIdReuseCache[xtype] = [id];
+            } else {
+                Ext.autoIdReuseCache[xtype].push(id);
+            }
+        }
+        return me.callParent(arguments);
+    }
+});
+
+Ext.define('Ext4App.Application', {
+    name: 'Ext4App',
+
+    extend: 'Ext.app.Application',
+
+    requires: [
+        'Ext.view.Table'
+    ],
+
+    views: [
+        // TODO: add views here
+    ],
+
+    controllers: [
+        // TODO: add controllers here
+    ],
+
+    stores: [
+        // TODO: add stores here
+    ],
+
+    init: function () {
+
+        Ext.reuseAutoIds = true;
+
+        /**
+         * Override of Ext.removeNode method to fix IE memory leak issues caused el and dom
+         * properties in the Ext.cache
+         */
+        Ext.removeNode = Ext.isIE
+            ? (function () {
+                var d;
+                return function (n) {
+                    if (n && n.tagName.toUpperCase() != 'BODY') {
+                        (Ext.enableNestedListenerRemoval) ? Ext.EventManager.purgeElement(n) : Ext.EventManager.removeAll(n);
+
+                        var cache = Ext.cache,
+                            id = n.id;
+
+                        if (cache[id]) {
+                            cache[id].el = null;
+                            cache[id].dom = null;
+                            delete cache[id];
+                        }
+
+                        if (n.parentNode) {
+                            n.parentNode.removeChild(n);
+                        }
+                        d = d || document.createElement('div');
+                        d.appendChild(n);
+                        d.innerHTML = '';
+                    }
+                };
+            }())
+            : function (n) {
+                if (n && n.parentNode && n.tagName.toUpperCase() != 'BODY') {
+                    (Ext.enableNestedListenerRemoval) ? Ext.EventManager.purgeElement(n) : Ext.EventManager.removeAll(n);
+
+                    var cache = Ext.cache,
+                        id = n.id;
+
+                    if (cache[id]) {
+                        cache[id].el = null;
+                        cache[id].dom = null;
+                        delete cache[id];
+                    }
+
+                    n.parentNode.removeChild(n);
+                }
+            };
+    },
+
+    launch: function () {
+        Ext.create('Ext.data.Store', {
+            storeId: 'simpsonsStore',
+            fields: ['name', 'email', 'phone'],
+            data: {
+                'items': [
+                    { 'name': 'Lisa', "email": "lisa@simpsons.com", "phone": "555-111-1224" },
+                    { 'name': 'Bart', "email": "bart@simpsons.com", "phone": "555-222-1234" },
+                    { 'name': 'Homer', "email": "home@simpsons.com", "phone": "555-222-1244" },
+                    { 'name': 'Marge', "email": "marge@simpsons.com", "phone": "555-222-1254" }
+                ]
+            },
+            proxy: {
+                type: 'memory',
+                reader: {
+                    type: 'json',
+                    root: 'items'
+                }
+            }
+        });
+
+        // override Ext.view.Table's rowTpl by removing the 'id'
+        if (Ext.Array.contains(location.search.toLowerCase().substring(1).split('&'), 'hiderowids')) {
+            Ext.view.Table.override({
+                rowTpl: [
+                    '{%',
+                    'var dataRowCls = values.recordIndex === -1 ? "" : " ' + Ext.baseCSSPrefix + 'grid-data-row";',
+                    '%}',
+                    // '<tr {[values.rowId ? ("id=\\"" + values.rowId + "\\"") : ""]} ',
+                    '<tr ',
+                    'data-boundView="{view.id}" ',
+                    'data-recordId="{record.internalId}" ',
+                    'data-recordIndex="{recordIndex}" ',
+                    'class="{[values.itemClasses.join(" ")]} {[values.rowClasses.join(" ")]}{[dataRowCls]}" ',
+                    '{rowAttr:attributes} tabIndex="-1" {ariaRowAttr}>',
+                    '<tpl for="columns">' +
+                    '{%',
+                    'parent.view.renderCell(values, parent.record, parent.recordIndex, parent.rowIndex, xindex - 1, out, parent)',
+                    '%}',
+                    '</tpl>',
+                    '</tr>',
+                    {
+                        priority: 0
+                    }
+                ],
+            });
+        }
+    }
+});
---
+++ sampleapps/ext426app/app/view/Main.js
@@ -0,0 +1,154 @@
+Ext.define('Ext4App.view.Main', {
+    extend: 'Ext.container.Container',
+    requires: [
+        'Ext.tab.Panel',
+        'Ext.layout.container.Border',
+        'Ext.grid.Panel',
+        'Ext.form.field.Display'
+    ],
+
+    xtype: 'app-main',
+
+    layout: {
+        type: 'border'
+    },
+
+    initComponent: function () {
+        this.numIterations = 30000; // the max number of iterations
+        this.numToggles = 0;
+
+        this.items = this.getItemsCfg();
+        this.callParent(arguments);
+    },
+
+    toggle: function () {
+        this.removeFromCenterPanel();
+        this.addToCenterPanel();
+
+        this.numToggles++;
+        var toggle = Ext.getCmp('numToggles');;
+        toggle.setValue(this.numToggles);
+    },
+
+    removeFromCenterPanel: function () {
+        var appMain = Ext.ComponentQuery.query('app-main')[0],
+            center = appMain.down('#center');
+
+        center.removeAll(true);
+    },
+
+    addToCenterPanel: function () {
+        var appMain = Ext.ComponentQuery.query('app-main')[0],
+            center = appMain.down('#center');
+
+        // make sure everything is removed
+        center.removeAll(true);
+
+        // now add
+        center.add({
+            xtype: 'gridpanel',
+            title: 'Simpsons',
+            store: Ext.data.StoreManager.lookup('simpsonsStore'),
+            columns: [
+                {
+                    header: 'id of row', // done via css in index.html
+                    width: 200
+                },
+                {
+                    header: 'Name',
+                    dataIndex: 'name',
+                    editor: 'textfield'
+                },
+                {
+                    header: 'Email',
+                    dataIndex: 'email',
+                    editor: 'textfield'
+                },
+                {
+                    header: 'Phone',
+                    dataIndex: 'phone',
+                    editor: 'textfield'
+                }
+            ]
+        });
+    },
+
+    runner: function () {
+        this.toggle();
+    },
+
+    startTask: function (reuseIds) {
+
+        Ext.reuseAutoIds = reuseIds;
+
+        if (!this.taskRunner) {
+            this.taskRunner = new Ext.util.TaskRunner();
+            this.task = this.taskRunner.newTask({
+                run: this.runner,
+                scope: this,
+                interval: 50,
+                repeat: this.numIterations
+            });
+        }
+
+        this.task.stop();
+        this.task.start();
+    },
+
+    stopTask: function () {
+        this.task.stop();
+    },
+
+    getItemsCfg: function () {
+        return [
+            {
+                region: 'west',
+                xtype: 'panel',
+                layout: {
+                    type: 'vbox',
+                    align: 'stretch'
+                },
+                title: 'west',
+                width: 150,
+                defaults: {
+                    xtype: 'button'
+                },
+                items: [
+                    {
+                        xtype: 'displayfield',
+                        fieldLabel: 'Iteration #',
+                        id: 'numToggles',
+                        value: 0
+                    },
+                    {
+                        text: 'Start New Ids',
+                        handler: Ext.bind(this.startTask, this, [false])
+                    },
+                    {
+                        text: 'Start Reuse Ids',
+                        handler: Ext.bind(this.startTask, this, [true])
+                    },
+                    {
+                        text: 'Stop',
+                        handler: Ext.bind(this.stopTask, this, [true])
+                    },
+                    {
+                        text: 'Remove',
+                        handler: this.removeFromCenterPanel
+                    },
+                    {
+                        text: 'Add',
+                        handler: this.addToCenterPanel
+                    }
+                ]
+            },
+            {
+                region: 'center',
+                itemId: 'center',
+                xtype: 'container',
+                layout: 'card',
+                items: [] // dynamically added
+            }
+        ];
+    }
+});
